240c6f84fdb172b6460ec0fa9d1a5c087e99b4dc
CKTim <471497226@qq.com>
2017-02-04 20:31:54 +0800

    second

--- app/src/main/java/com/cxk/wechatlog/WeChatLogService.java
+++ app/src/main/java/com/cxk/wechatlog/WeChatLogService.java
@@ -13,16 +13,20 @@ import java.util.List;
 /**
  * Created by cxk on 2017/2/4.
  * email:471497226@qq.com
- *
+ * <p>
  * 获取即时微信聊天记录服务类
  */
 
 public class WeChatLogService extends AccessibilityService {
 
     /**
-     * 过滤使用，防止重复记录的打印
+     * 聊天对象
      */
-    private String isRepeat = "cxk";
+    private String ChatName;
+    /**
+     * 聊天最新一条记录
+     */
+    private String ChatRecord = "test";
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
@@ -49,25 +53,37 @@ public class WeChatLogService extends AccessibilityService {
         if (rootNode != null) {
             //获取所有聊天的线性布局
             List<AccessibilityNodeInfo> listChatRecord = rootNode.findAccessibilityNodeInfosByViewId("com.tencent.mm:id/o");
+            if(listChatRecord.size()==0){
+                return;
+            }
             //获取最后一行聊天的线性布局（即是最新的那条消息）
             AccessibilityNodeInfo finalNode = listChatRecord.get(listChatRecord.size() - 1);
-            //获取聊天对象
+            //获取聊天对象list（其实只有size为1）
             List<AccessibilityNodeInfo> imageName = finalNode.findAccessibilityNodeInfosByViewId("com.tencent.mm:id/i_");
-            //获取聊天信息
+            //获取聊天信息list（其实只有size为1）
             List<AccessibilityNodeInfo> record = finalNode.findAccessibilityNodeInfosByViewId("com.tencent.mm:id/ib");
             if (imageName.size() != 0) {
                 if (record.size() == 0) {
-                    if (!isRepeat.equals("对方发的是图片或者表情")) {
-                        Log.e("AAAA", imageName.get(0).getContentDescription().toString().replace("头像", "") + "：" + "对方发的是图片或者表情");
-                        Toast.makeText(this,imageName.get(0).getContentDescription().toString().replace("头像", "") + "：" + "对方发的是图片或者表情", Toast.LENGTH_SHORT).show();
-                        isRepeat = "对方发的是图片或者表情";
+                    //判断当前这条消息是不是和上一条一样，防止重复
+                    if (!ChatRecord.equals("对方发的是图片或者表情")) {
+                        //获取聊天对象
+                        ChatName = imageName.get(0).getContentDescription().toString().replace("头像", "");
+                        //获取聊天信息
+                        ChatRecord = "对方发的是图片或者表情";
+
+                        Log.e("AAAA", ChatName + "：" + "对方发的是图片或者表情");
+                        Toast.makeText(this, ChatName + "：" + ChatRecord, Toast.LENGTH_SHORT).show();
                     }
                 } else {
                     //判断当前这条消息是不是和上一条一样，防止重复
-                    if (!isRepeat.equals(record.get(0).getText().toString())) {
-                        Log.e("AAAA", imageName.get(0).getContentDescription().toString().replace("头像", "") + "：" + record.get(0).getText().toString());
-                        Toast.makeText(this,imageName.get(0).getContentDescription().toString().replace("头像", "") + "：" + record.get(0).getText().toString(), Toast.LENGTH_SHORT).show();
-                        isRepeat = record.get(0).getText().toString();
+                    if (!ChatRecord.equals(record.get(0).getText().toString())) {
+                        //获取聊天对象
+                        ChatName = imageName.get(0).getContentDescription().toString().replace("头像", "");
+                        //获取聊天信息
+                        ChatRecord = record.get(0).getText().toString();
+
+                        Log.e("AAAA", ChatName + "：" + ChatRecord);
+                        Toast.makeText(this, ChatName + "：" + ChatRecord, Toast.LENGTH_SHORT).show();
                     }
                 }
             }
