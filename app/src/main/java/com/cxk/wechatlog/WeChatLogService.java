36ee4ed0a7c803ffd9f72208fe0178895934274c
CKTim <471497226@qq.com>
2017-02-07 09:32:07 +0800

    微信监听Demo，2.7更新

--- app/src/main/java/com/cxk/wechatlog/WeChatLogService.java
+++ app/src/main/java/com/cxk/wechatlog/WeChatLogService.java
@@ -9,6 +9,7 @@ import android.view.accessibility.AccessibilityNodeInfo;
 import android.widget.Toast;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 /**
  * Created by cxk on 2017/2/4.
@@ -27,6 +28,11 @@ public class WeChatLogService extends AccessibilityService {
      * 聊天最新一条记录
      */
     private String ChatRecord = "cxk";
+
+    /**
+     * 小视频的秒数，格式为00:00
+     */
+    private String VideoSecond;
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent event) {
@@ -66,43 +72,107 @@ public class WeChatLogService extends AccessibilityService {
     }
 
     /**
-     * 遍历所有控件，找到联系人，头像Imagview里面有
+     * 遍历所有控件，找到头像Imagview，里面有对联系人的描述
      */
     private void GetChatName(AccessibilityNodeInfo node) {
         for (int i = 0; i < node.getChildCount(); i++) {
             AccessibilityNodeInfo node1 = node.getChild(i);
             if ("android.widget.ImageView".equals(node1.getClassName()) && node1.isClickable()) {
-                //获取聊天对象
-                ChatName = node1.getContentDescription().toString().replace("头像", "");
+                //获取聊天对象,这里两个if是为了确定找到的这个ImageView是头像的
+                if (!TextUtils.isEmpty(node1.getContentDescription())) {
+                    ChatName = node1.getContentDescription().toString();
+                    if (ChatName.contains("头像")) {
+                        ChatName = ChatName.replace("头像", "");
+                    }
+                }
+
             }
             GetChatName(node1);
         }
     }
 
     /**
-     * 遍历所有控件，找到TextView，因为里面只有一个Textview装载着聊天信息
+     * 遍历所有控件:这里分四种情况
+     * 文字聊天: 一个TextView，并且他的父布局是android.widget.RelativeLayout
+     * 语音的秒数: 一个TextView，并且他的父布局是android.widget.RelativeLayout，但是他的格式是0"的格式，所以可以通过这个来区分
+     * 图片:一个ImageView,并且他的父布局是android.widget.FrameLayout,描述中包含“图片”字样（发过去的图片），发回来的图片现在还无法监听
+     * 表情:也是一个ImageView,并且他的父布局是android.widget.LinearLayout
+     * 小视频的秒数:一个TextView，并且他的父布局是android.widget.FrameLayout，但是他的格式是00:00"的格式，所以可以通过这个来区分
      *
      * @param node
      */
     public void GetChatRecord(AccessibilityNodeInfo node) {
         for (int i = 0; i < node.getChildCount(); i++) {
-            AccessibilityNodeInfo node1 = node.getChild(i);
-            if ("android.widget.TextView".equals(node1.getClassName())) {
-                //获取聊天记录
-                String chatRecord1 = node1.getText().toString();
-                //检查下是不是重复消息，防止多次显示
-                if (!TextUtils.isEmpty(chatRecord1)) {
-                    if (!chatRecord1.equals(ChatRecord)) {
-                        ChatRecord = chatRecord1;
-                        Toast.makeText(this, ChatName + ":" + ChatRecord, Toast.LENGTH_SHORT).show();
+            AccessibilityNodeInfo nodeChild = node.getChild(i);
+
+            //聊天内容是:文字聊天(包含语音秒数)
+            if ("android.widget.TextView".equals(nodeChild.getClassName()) && "android.widget.RelativeLayout".equals(nodeChild.getParent().getClassName().toString())) {
+                if (!TextUtils.isEmpty(nodeChild.getText())) {
+                    String RecordText = nodeChild.getText().toString();
+                    //这里加个if是为了防止多次触发TYPE_VIEW_SCROLLED而打印重复的信息
+                    if (!RecordText.equals(ChatRecord)) {
+                        ChatRecord = RecordText;
+                        //判断是语音秒数还是正常的文字聊天,语音的话秒数格式为5"
+                        if (ChatRecord.contains("\"")) {
+                            Toast.makeText(this, ChatName + "发了一条" + ChatRecord + "的语音", Toast.LENGTH_SHORT).show();
+
+                            Log.e("WeChatLog",ChatName + "发了一条" + ChatRecord + "的语音");
+                        } else {
+                            //这里在加多一层过滤条件，确保得到的是聊天信息，因为有可能是其他TextView的干扰，例如名片等
+                            if (nodeChild.isLongClickable()) {
+                                Toast.makeText(this, ChatName + "：" + ChatRecord, Toast.LENGTH_SHORT).show();
+
+                                Log.e("WeChatLog",ChatName + "：" + ChatRecord);
+                            }
+
+                        }
                         return;
                     }
                 }
-                GetChatRecord(node1);
             }
 
+            //聊天内容是:表情
+            if ("android.widget.ImageView".equals(nodeChild.getClassName()) && "android.widget.LinearLayout".equals(nodeChild.getParent().getClassName().toString())) {
+                Toast.makeText(this, ChatName+"发的是表情", Toast.LENGTH_SHORT).show();
+
+                Log.e("WeChatLog",ChatName+"发的是表情");
+
+                return;
+            }
+
+            //聊天内容是:图片
+            if ("android.widget.ImageView".equals(nodeChild.getClassName())) {
+                //安装软件的这一方发的图片（另一方发的暂时没实现）
+                if("android.widget.FrameLayout".equals(nodeChild.getParent().getClassName().toString())){
+                    if(!TextUtils.isEmpty(nodeChild.getContentDescription())){
+                        if(nodeChild.getContentDescription().toString().contains("图片")){
+                            Toast.makeText(this, ChatName+"发的是图片", Toast.LENGTH_SHORT).show();
+
+                            Log.e("WeChatLog",ChatName+"发的是图片");
+                        }
+                    }
+                }
+            }
+
+            //聊天内容是:小视频秒数,格式为00：00
+            if ("android.widget.TextView".equals(nodeChild.getClassName()) && "android.widget.FrameLayout".equals(nodeChild.getParent().getClassName().toString())) {
+                if (!TextUtils.isEmpty(nodeChild.getText())) {
+                    String second = nodeChild.getText().toString().replace(":", "");
+                    //正则表达式，确定是不是纯数字,并且做重复判断
+                    if (second.matches("[0-9]+") && !second.equals(VideoSecond)) {
+                        VideoSecond = second;
+                        Toast.makeText(this, ChatName + "发了一段" + nodeChild.getText().toString() + "的小视频", Toast.LENGTH_SHORT).show();
+
+                        Log.e("WeChatLog","发了一段" + nodeChild.getText().toString() + "的小视频");
+                    }
+                }
+
+            }
+
+            GetChatRecord(nodeChild);
         }
     }
+
 
     /**
      * 必须重写的方法：系统要中断此service返回的响应时会调用。在整个生命周期会被调用多次。
